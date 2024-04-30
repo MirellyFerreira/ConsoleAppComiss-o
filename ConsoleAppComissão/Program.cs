d45ce99583d2874bde5e7012cbd1bbe85b1e3af0
Usuario <Usuario@CPD02-M12>
2024-04-30 12:23:53 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppComissão/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppComissão
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new String[5];
+            double[] peso = new double[5];
+            double[] altura = new double[5];
+
+            for (int i = 0; i <= 5; i++) 
+            {
+                Console.WriteLine("Informe o nome da pessoa: ");
+                nome[i] = Console.ReadLine();
+                Console.WriteLine("informe o peso da pessoa: ");
+                peso[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine("informe a altura da pessoa: ");
+                altura[i] = double.Parse(Console.ReadLine());
+
+                
+                {
+                    for (int j = 0; j <= 4 j++)
+                    
+                {
+                        Console.WriteLine("Vendedor: " + vendedor[j] + "valor da venda: " + venda[j].ToString("C") + "valor da comissão: " + comissao[j].ToString("C"));
+
+                           
+                    
+                    }
+                }
+            }
+
+        }
+    }
+}
